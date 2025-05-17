fb261109a18c904fd96f7a71620214790ad97a43
Juan <david.2727@gmail.com>
2025-05-16 22:44:17 -0500

    Primer commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import 'dotenv/config';
+import cors from 'cors';
+import noticiasRouter from "../src/routers/noticiasRouter.js"
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use("/noticias", noticiasRouter);
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

7c618e8999f767495a381ec0ac0bc33e527308e5
Zulu <juanpablozuluaga.0311@gmail.com>
2025-05-27 09:30:46 -0500

    Cambios de validación controlador.

--- src/controllers/noticiasController.js
+++ src/controllers/noticiasController.js
@@ -1,61 +1,60 @@
-import * as noticiasService from "../service/noticiasService.js"
+import * as noticiasService from "../service/noticiasService.js";
 
-export const crearNoticia = async(req, res) => {
-    try {
-        const{data, error} = await noticiasService.crearNoticia(req.body);
-        if(error) return res.status(400).json(error);
-        res.status(201).json(data)
-    } catch (error) {
-        res.status(400).json({message: error.message});
-    }
-}
-
-export const getNoticias = async(req, res) => {
-    try {
-        const {data, error} = await noticiasService.getNoticias();
-        if(error) return res.status(400).json(error);
-        res.status(200).json(data);
-    } catch (error) {
-        res.status(400).json({message:error.message})
-    }
-}
-
-export const editarNoticia = async(req, res) => {
-    try {
-        const {id} = req.params;
-        const {data, error} = await noticiasService.editarNoticia(req.body, id);
-        if(error) return res.status(400).json(error);
-        res.status(201).json(data);
-    } catch (error) {
-        res.status(400).json({message: error.message});
-    }
-}
-
-export const getNoticiaById = async(req, res) => {
-    try {
-        const { id } = req.params;
-        const { data, error } = await noticiasService.getNoticiaById(id);
-
-        if (error) return res.status(400).json({ message: error });
-
-        if (!data) {
-            return res.status(404).json({ message: "Noticia no encontrada" });
-        }
-
-        res.status(200).json(data);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+export const crearNoticia = async (req, res) => {
+  try {
+    const { data, error } = await noticiasService.crearNoticia(req.body);
+    if (error) return res.status(400).json(error);
+    res.status(201).json(data);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 };
 
+export const getNoticias = async (req, res) => {
+  try {
+    const { data, error } = await noticiasService.getNoticias();
+    if (error) return res.status(400).json(error);
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
 
-export const eliminarNoticia = async(req, res) => {
-    try {
-        const {id} = req.params;
-        const {error} = await noticiasService.eliminarNoticia(id);
-        if(error) return res.status(400).json("!!"+error);
-        res.status(200).json({message: "Noticia eliminada con éxito"});
-    } catch (error) {
-        res.status(400).json({message: error.message});
+export const editarNoticia = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { data, error } = await noticiasService.editarNoticia(req.body, id);
+    if (error) return res.status(400).json(error);
+    res.status(201).json(data);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+export const getNoticiaById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { data, error } = await noticiasService.getNoticiaById(id);
+
+    if (error) return res.status(400).json({ message: error });
+
+    if (error || !data) {
+      return res.status(404).json({ message: "Noticia no encontrada" + data });
     }
-}
+
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const eliminarNoticia = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { error } = await noticiasService.eliminarNoticia(id);
+    if (error) return res.status(400).json("!!" + error);
+    res.status(200).json({ message: "Noticia eliminada con éxito" });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};

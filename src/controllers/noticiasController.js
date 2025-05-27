a7f6559e50bfaae3ed60013b2f9b77a7dd90f5e8
Zulu <juanpablozuluaga.0311@gmail.com>
2025-05-27 09:15:53 -0500

    Controllador cambiado

--- src/controllers/noticiasController.js
+++ src/controllers/noticiasController.js
@@ -33,14 +33,21 @@ export const editarNoticia = async(req, res) => {
 
 export const getNoticiaById = async(req, res) => {
     try {
-        const {id} = req.params;
-        const {data, error} = await noticiasService.getNoticiaById(id);
-        if(error) return res.status(400).json(error);
+        const { id } = req.params;
+        const { data, error } = await noticiasService.getNoticiaById(id);
+
+        if (error) return res.status(400).json({ message: error });
+
+        if (!data) {
+            return res.status(404).json({ message: "Noticia no encontrada" });
+        }
+
         res.status(200).json(data);
     } catch (error) {
-        res.status(400).json({message: error.message});
+        res.status(500).json({ message: error.message });
     }
-}
+};
+
 
 export const eliminarNoticia = async(req, res) => {
     try {
